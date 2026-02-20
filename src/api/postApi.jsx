48c3d22a18bdf1d75a049a913cf22d5cf4e468c4
Raj Mahajan <mvraj7898@gmail.com>
2026-02-20 23:55:00 +0530

    post api

--- src/api/postApi.jsx
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-const api = axios.create({
-  baseURL: "https://restcountries.com/v3.1",
-});
-
-export const getCountriesData = () => {
-  return api.get("/all?fields=name,population,region,capital,flags");
-};
